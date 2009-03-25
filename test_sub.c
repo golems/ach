6113ce128b093fb2a035464f36ee3cd6a5a1daeb
Neil T. Dantam <ntd@gatech.edu>
2009-03-25 02:31:55 +0000

    Big commit of probably working code.  Implemented and tested publishing of fixed size frames and subscribing with retrieving of latest frame.  Still need to implement retrieving of next frame in sequence.
    git-svn-id: file:///home/svn/humanoids/ach/trunk@584 4f504ea5-f356-0410-adec-f1b3679f3bb7

---
+++ test_sub.c
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2008, Georgia Tech Research Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ *     * Neither the name of the Georgia Tech Research Corporation nor
+ *       the names of its contributors may be used to endorse or
+ *       promote products derived from this software without specific
+ *       prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY GEORGIA TECH RESEARCH CORPORATION ''AS
+ * IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL GEORGIA
+ * TECH RESEARCH CORPORATION BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/** \file ach.c
+ *  \author Neil T. Dantam
+ */
+
+//#define _GNU_SOURCE
+
+#include <stdint.h>
+#include <pthread.h>
+#include <stdio.h>
+#include "ach.h"
+
+#include <stdint.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <ctype.h>
+
+#include <string.h>
+
+
+
+
+
+int main(int argc, char **argv) {
+    ach_channel_t chan;
+    int r;
+
+    r = ach_subscribe( &chan, "ach-test" );
+    fprintf(stderr, "Subscribe: %s\n", ach_result_to_string( r ) );
+
+    for( int i = 0;  i < 20; i++ ) {
+        uint16_t v;
+        r = ach_get_last( &chan, &v, sizeof(uint16_t) );
+        if(  ACH_STALE != r )
+            fprintf(stderr, "Read %d: %s\n", v, ach_result_to_string( r ) );
+        sleep(1);
+    }
+
+    r = ach_close( &chan );
+    fprintf(stderr, "Close: %s\n", ach_result_to_string( r ) );
+
+    if( ACH_FAILED_SYSCALL == r )
+        perror("Close Fail Reason: ");
+    return 0;
+}
