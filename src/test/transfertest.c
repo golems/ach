66dbfed7e244f6efd3caedb08edd9514c135bd8e
Neil T. Dantam <ntd@gatech.edu>
2014-04-30 00:41:13 -0400

    Test transfer function that mallocs

--- src/test/transfertest.c
+++ src/test/transfertest.c
@@ -53,7 +53,6 @@
 #include <sched.h>
 #include <pthread.h>
 #include <stdio.h>
-#include <assert.h>
 #include "ach.h"
 
 #define OPT_CHAN  "ach-test-transfer"
@@ -94,7 +93,7 @@ static void check_cpy() {
     /* Put Data */
     size_t n = strlen(data)+1;
     r = ach_xput( &channel, ach_put_fun_memcpy, &n, data, n );
-    assert(ACH_OK == r );
+    test(r, "memcpy put");
 
     /* Get Data */
     size_t max_n = sizeof(buf);
@@ -103,11 +102,47 @@ static void check_cpy() {
     r = ach_xget( &channel,
                   &ach_get_fun_memcpy, &max_n, &pbuf,
                   &frame_size, NULL, 0 );
-    assert(ACH_OK == r );
+    test(r, "memcpy get");
 
     /* Check Data */
-    assert(0 == strcmp(data, buf) );
+    if( strcmp(data, buf) ) exit(-1);
 }
+
+/* --- MALLOC TEST ---*/
+
+enum ach_status
+ach_get_fun_malloc(void *cx, void **obj_dst, const void *chan_src, size_t frame_size )
+{
+    (void)cx;
+    *obj_dst = malloc(frame_size);
+    memcpy( *obj_dst, chan_src, frame_size );
+    return ACH_OK;
+}
+
+static void check_malloc() {
+    /* Generate Data */
+    enum ach_status r;
+    char data[]="malloc321";
+
+    /* Put Data */
+    size_t n = strlen(data)+1;
+    r = ach_xput( &channel, ach_put_fun_memcpy, &n, data, n );
+    test(r, "malloc put");
+
+    /* Get Data */
+    size_t frame_size;
+    void *pbuf;
+    r = ach_xget( &channel,
+                  &ach_get_fun_malloc, NULL, &pbuf,
+                  &frame_size, NULL, 0 );
+    test(r, "malloc get");
+
+    /* Check Data */
+    if( strcmp(data, pbuf) ) exit(-1);
+    free(pbuf);
+}
+
+/* --- Main ---*/
 
 int main( int argc, char **argv ){
 
@@ -133,7 +168,7 @@ int main( int argc, char **argv ){
 
 
     check_cpy();
+    check_malloc();
 
     return 0;
-
 }
